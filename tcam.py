eeec99fc9d0c4abc22b89413ec058a6865f80b39
Thomas Luinaud <thomas.luinaud@gmail.com>
2019-02-19 16:22:57 -0500

    add comment

--- tcam.py
+++ tcam.py
@@ -1,23 +1,45 @@
 import sys
 class tcam:
+    """ a basic tcam class
+    
+    """
     def __init__(self,size=sys.maxsize):
+        """
+        size : max number of entries
+        TODO : add the possibility to constraints entry width
+        """
         self.MaxEntries=size
         self.content=[]
         
     def insert(self,key, mask, pri, val, addr=None):
+        """insert information
+        key : key to look
+        mask : mask of the entry
+        pri : priority
+        val : result
+        addr : position inside the TCAM : optional
+        """
         if len(self.content) < self.MaxEntries:
             self.content.append((key,mask,pri,val,addr))
         else:
             print("memory full")
     
     def search(self,val):
+        """
+        return the value associtated to val with the highest priority
+        if two match have the same priority take the first one found
+        TODO: better algorithm for search
+        """
         res=None
         for (mask,key,pri,resO,_) in self.content:
             if (key & mask) == (val & mask) and res[1]<pri:
                 res = (resO, pri)
-        return res
+        return res[0]
         
     def delete(self, addr):
+        """
+        delete the entry at addr
+        """
         i=0
         for (_,_,_,_,elem) in self.content:
             if addr==elem:
@@ -26,6 +48,9 @@ class tcam:
             i=i+1
         
     def delete(self, key, mask):
+        """
+        delete the entry corresponding key, mask
+        """
         i=0
         for (mask, key, _,_) in self.content:
             if mask==mask and key==key:
@@ -34,6 +59,10 @@ class tcam:
                 i=i+1
 
     def print(self):
+        """
+        simple print of the memory content
+        TODO: look at a way for a better format
+        """
         print("number of entries ", len(self.content))
         print(self.content)
     
