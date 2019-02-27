3a4c41e092a2765fcff2c7a1df4011b67103fe53
Thomas Luinaud <thomas.luinaud@gmail.com>
2019-02-27 16:19:17 -0500

    add validation when inserting values

--- tcam.py
+++ tcam.py
@@ -1,16 +1,28 @@
 import sys
+import math
+import warnings
 class tcam:
     """ a basic tcam class
     
     """
-    def __init__(self,entryWidth, size=sys.maxsize):
+    def __init__(self,entryWidth, priWidth=8, addrWidth=int(math.log2(sys.maxsize)), valueWidth=32, size=sys.maxsize):
         """
+        entryWidth : width in bits of the entry
+        priWidth : Width of the priority in bits
+        addrWidth : width of addresses in bits
+        valueWidth : width of the associated value in bit
         size : max number of entries
-        TODO : add the possibility to constraints entry width
+        entryWidth : size of an entry in bits
+        
         """
+        if math.log2(size) > addrWidth :
+            warnings.warn("addr width can't represents the size of table")
         self.MaxEntries=size
+        self.PriorityWidth=priWidth
+        self.AddrWidth=addrWidth
+        self.ValueWidth=valueWidth
         self.EntryWidth=entryWidth
-        self.content=[]
+        self.Content=[]
         
     def insert(self,key, mask, pri, val, addr=None):
         """insert information
@@ -20,10 +32,22 @@ class tcam:
         val : result
         addr : position inside the TCAM : optional
         """
-        if len(self.content) < self.MaxEntries:
-            self.content.append((key,mask,pri,val,addr))
-        else:
-            print("memory full")
+        line=(key,mask,pri,val,addr)
+
+        if len(self.Content) >= self.MaxEntries:
+            raise MemoryError("memory full content {} not inserted".format(line))
+        if key != 0 and math.log2(key) > self.EntryWidth:
+            raise ValueError("inserted key {} too large".format(key))
+        if mask != 0 and math.log2(mask) > self.EntryWidth:
+            raise ValueError("inserted mask {} too large".format(mask))
+        if pri!= 0 and math.log2(pri) > self.PriorityWidth:
+            raise ValueError("inserted priority key {} too large".format(pri))
+        if val != 0 and math.log2(val) > self.ValueWidth:
+            raise ValueError("inserted value {} too large".format(val))
+        
+        self.Content.append(line)
+        print("content {} inserted".format(line))
+
     
     def search(self,val):
         """
@@ -32,7 +56,7 @@ class tcam:
         TODO: better algorithm for search
         """
         res=(0,-1)
-        for (key,mask,pri,resO,_) in self.content:
+        for (key,mask,pri,resO,_) in self.Content:
             if (key & ~mask) == (val & ~mask) and  res[1]<pri:
                 res = (resO, pri)
         if res==(0,-1):
@@ -45,9 +69,9 @@ class tcam:
         delete the entry at addr
         """
         i=0
-        for (_,_,_,_,elem) in self.content:
+        for (_,_,_,_,elem) in self.Content:
             if addr==elem:
-                del self.content[i]
+                del self.Content[i]
                 break
             i=i+1
         
@@ -56,9 +80,9 @@ class tcam:
         delete the entry corresponding key, mask
         """
         i=0
-        for (mask, key, _,_) in self.content:
+        for (mask, key, _,_) in self.Content:
             if mask==mask and key==key:
-                del self.content[i]
+                del self.Content[i]
             else:
                 i=i+1
 
@@ -69,8 +93,8 @@ class tcam:
         """
         find_all = lambda c,s: [x for x in range(c.find(s), len(c)) if c[x] == s]
         printFormat='{{0:0{0}b}}'.format(self.EntryWidth)
-        print("number of entries ", len(self.content))
-        for (key,mask,pri,res,_) in self.content:
+        print("number of entries ", len(self.Content))
+        for (key,mask,pri,res,_) in self.Content:
             #print("key  : {}".format(printFormat.format(key)))
             #print("mask : {}".format(printFormat.format(mask)))
             l=list(printFormat.format(key))
--- tcam_test.py
+++ tcam_test.py
@@ -2,8 +2,10 @@
 from tcam import tcam
 
 def main():
-    mem=tcam(16)
+    mem=tcam(16,8,5,32,2)
     mem.insert(5,3,0,4,256)
+    mem.insert(9,3,0,15,256)
+#    mem.insert(5,3,0,4,256)
     mem.print()
     print(mem.search(5))
     print(mem.search(4))
